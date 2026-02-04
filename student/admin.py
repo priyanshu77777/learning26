421da3e49383247326c6dc37ae58ad0ad5d84bab
Priyanshu Modi <pumodi3115@gmail.com>
2026-02-04 12:24:13 +0530

    ORM

--- student/admin.py
+++ student/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Student,Product,Car
 # Register your models here.
+
+admin.site.register(Student)
+admin.site.register(Product)
+admin.site.register(Car)
--- student/models.py
+++ student/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+class Student(models.Model):
+    studentName = models.CharField(max_length=100)
+    studentAge = models.IntegerField()
+    studentCity = models.CharField(max_length=40)
+    studentEmail = models.EmailField(null=True)
+
+    class Meta:
+        db_table = "student"
+
+class Product(models.Model):
+    productName = models.CharField(max_length=100)
+    productPrice = models.IntegerField()
+    productDescription = models.TextField()
+    productStock = models.PositiveBigIntegerField()
+    productColor = models.CharField(max_length=20,null=True)
+    productStatus = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = "product"
+
+class Car(models.Model):
+    carName = models.CharField(max_length=25)
+    carPrice = models.IntegerField()
+    carStock = models.PositiveBigIntegerField()
+    carColor = models.CharField(max_length=30,null=True)
